d95e7f4ada34dbaa14f1a8a333d1dbbf6d474837
DwiAndriyani <dwiandriyanisunardiii@gmail.com>
2024-12-19 15:34:28 +0900

    Tambahkah file index

---
+++ index.php
@@ -0,0 +1,105 @@
+<?php
+session_start();
+include "koneksi/ceksession.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Arsip Surat Kemenag </title>
+    <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+    <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+    
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:400,300|Raleway:300,400,900,700italic,700,300,600">
+    <link rel="stylesheet" type="text/css" href="css/jquery.bxslider.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="shortcut icon" href="img/icon.ico">
+    <style>
+      img{
+        width: 20%;
+        height:20%;
+      }
+      .header{
+        background: url('img/kk.jpg') no-repeat fixed;
+        background-size: cover;
+        min-height: 650px;
+        position: relative;
+      }
+    </style>
+
+  </head>
+  <body>
+
+    <div class="loader"></div>
+    <div id="myDiv">
+    <!--HEADER-->
+    <div class="header">
+      <div class="bg-color">
+        <header id="main-header">
+        <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">ARSIP SURAT <span class="logo-dec">KANTOR KEMENTERIAN AGAMA</span></a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+              <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="#main-header">Beranda</a></li>
+                <ul class="nav navbar-nav navbar-right">
+                <li class="">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <img src="" alt="">Masuk
+                    <span class=" fa fa-angle-down"></span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                    <li><a href="admin/login"><i class="fa fa-sign-out pull-right"></i> Admin</a></li> 
+                    <li><a href="bagian/login"><i class="fa fa-sign-out pull-right"></i> Bagian</a></li>
+                  </ul>
+                </li>
+              </ul>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        </header>
+        <div class="wrapper">
+        <div class="container">
+          <div class="row">
+            <div class="banner-info text-center wow fadeIn delay-05s">
+              <h2 class="bnr-sub-title"></h2>
+              <div class="logo">
+		            <img src="img/d.png" alt="" />
+	            </div>
+              <h3 class="bnr-sub-title">SISTEM INFORMASI ARSIP SURAT </h3>
+                <h3 class="bnr-sub-title"><span class="logo-dec">KANTOR KEMENTERIAN AGAMA</span></h3>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    <!--/ HEADER-->
+   
+   
+    <!---->
+    <!---->
+   
+  </div>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/jquery.bxslider.min.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="contactform/contactform.js"></script>
+    
+  </body>
+</html>
